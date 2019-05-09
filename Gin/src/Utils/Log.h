431a1f9db1e4e61b2447318761877b1fac7794e4
Jared Ross <jdross91@gmail.com>
2019-05-08 20:56:50 -0600

    Add log example

--- Gin/src/Utils/Log.h
+++ Gin/src/Utils/Log.h
@@ -12,6 +12,8 @@ namespace Gin
 	public:
 		static void Init();
 
+		// For multiple args, use {} as a placeholder in the first arg and the remaining args are placed inside.
+		// Example CORE_INFO("This is a {}", "test") outputs: "This is a test"
 		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return coreLogger; }
 		inline static std::shared_ptr<spdlog::logger>& GetGameLogger() { return gameLogger; }
 
