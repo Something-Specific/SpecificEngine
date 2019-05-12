7f9230b83c890a7c0545b3bbe58652586e2c805a
Jared Ross <jdross91@gmail.com>
2019-05-12 13:44:44 -0600

    Adding in vendor includes
    spdlog
    nlohmann-json
    catch2

--- Sandbox/src/Application.cpp
+++ Sandbox/src/Application.cpp
@@ -1,14 +1,27 @@
 
 #include "Gin.h"
-#include "Game.h"
-#include "EventsExample.h"
+//#include "Game.h"
+//#include "EventsExample.h"
+#include <iostream>
+#include "json.hpp"
+
 
 int main(int argc, char *argv[])
 {
-	Gin::Log::Init();
-	
-	Gin::Core::MasterGame* game = new Demo::Game();
-	game->Run();
-	
+	nlohmann::json t = {
+		  {"pi", 3.141},
+		  {"happy", true},
+		  {"name", "Niels"},
+		  {"nothing", nullptr},
+		  {"answer", {
+			{"everything", 42}
+		  }},
+		  {"list", {1, 0, 2}},
+		  {"object", {
+			{"currency", "USD"},
+			{"value", 42.99}
+		  }}
+	};
+	std::cout << t << std::endl;
 	return 0;
 }
