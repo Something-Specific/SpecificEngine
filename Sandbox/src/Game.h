8dcd82fc8644e6399a58c87783260484a1244abc
Jared <jdross91@gmail.com>
2020-07-23 10:15:34 -0600

    Add override keyword as a best practice

--- Sandbox/src/Game.h
+++ Sandbox/src/Game.h
@@ -8,8 +8,8 @@ namespace Demo
 	class Game : public Gin::Core::MasterGame
 	{
 	public:
-		void Update(Gin::Inputs::Processor *processor, float dt);
-		void Render(Gin::Graphics::Renderer *renderer, float dt);
+		void Update(Gin::Inputs::Processor *processor, float dt) override;
+		void Render(Gin::Graphics::Renderer *renderer, float dt) override;
 
 	private:
 		void Init();
